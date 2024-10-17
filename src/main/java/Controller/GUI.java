f3306d777105553e983d55e176977c6403c25445
Emre EKMEL <emreekmel129@gmail.com>
2024-10-17 15:33:19 +0300

    İyileştirmeler

--- src/main/java/Controller/GUI.java
+++ src/main/java/Controller/GUI.java
@@ -113,6 +113,27 @@ public class GUI implements Initializable {
             e.printStackTrace();
         }
 
+        // TextField için aksiyon tanımlaması
+
+        /*searchfield.setOnAction(event -> {
+            String arananTarif = searchfield.getText();
+            Tarif bulunanTarif = DatabaseConnection.getTarifByName(arananTarif);
+
+
+
+            if (bulunanTarif != null) {
+                System.out.println(bulunanTarif.getTarifAdi());
+                System.out.println("buldun");
+                //showRecipeDetails(bulunanTarif);
+            } else {
+                //showAlert("Tarif bulunamadı!");
+                System.out.println("bulamadın");
+            }
+        });*/
+
+        // "Malzeme Ekle" butonuna tıklanınca formu aç
+        malzemeEkleButton.setOnAction(event -> showAddMalzemeDialog());
+
         //Arama butonuna tıklanınca
         searchbutton.setOnAction(event -> {
             // Arama alanındaki tarifi al
@@ -153,7 +174,60 @@ public class GUI implements Initializable {
         alert.showAndWait();
     }
 
+    // Malzeme ekleme formunu açan fonksiyon
+    // Malzeme ekleme formunu açan fonksiyon
+    private void showAddMalzemeDialog() {
+        Dialog<Malzeme> dialog = new Dialog<>();
+        dialog.setTitle("Malzeme Ekle");
+        dialog.setHeaderText("Yeni Malzeme Bilgilerini Girin");
 
+        // Dialog için butonlar
+        ButtonType ekleButtonType = new ButtonType("Ekle", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(ekleButtonType, ButtonType.CANCEL);
 
+        // Dialog içeriği
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(20, 150, 10, 10));
+
+        TextField malzemeAdiField = new TextField();
+        malzemeAdiField.setPromptText("Malzeme Adı");
+        TextField malzemeBirimField = new TextField();
+        malzemeBirimField.setPromptText("Malzeme Birimi");
+        TextField malzemeMiktarField = new TextField();
+        malzemeMiktarField.setPromptText("Malzeme Miktarı");
+        TextField malzemeFiyatField = new TextField();
+        malzemeFiyatField.setPromptText("Birim Fiyat");
+
+        gridPane.add(new Label("Malzeme Adı:"), 0, 0);
+        gridPane.add(malzemeAdiField, 1, 0);
+        gridPane.add(new Label("Malzeme Birimi:"), 0, 1);
+        gridPane.add(malzemeBirimField, 1, 1);
+        gridPane.add(new Label("Malzeme Miktarı:"), 0, 2);
+        gridPane.add(malzemeMiktarField, 1, 2);
+        gridPane.add(new Label("Birim Fiyat:"), 0, 3);
+        gridPane.add(malzemeFiyatField, 1, 3);
+
+        dialog.getDialogPane().setContent(gridPane);
+
+        dialog.setResultConverter(dialogButton -> {
+            if (dialogButton == ekleButtonType) {
+                String malzemeAdi = malzemeAdiField.getText();
+                String malzemeBirim = malzemeBirimField.getText();
+                int toplamMiktar = Integer.parseInt(malzemeMiktarField.getText());
+                float birimFiyat = Float.parseFloat(malzemeFiyatField.getText());
+
+                DatabaseConnection.addMalzeme(malzemeAdi, toplamMiktar, malzemeBirim, birimFiyat);
+
+                //return new Malzeme(malzemeAdi, toplamMiktar, malzemeBirim, birimFiyat);
+            }
+            return null;
+        });
+
+        dialog.showAndWait();
+    }
 
 }
+
+
